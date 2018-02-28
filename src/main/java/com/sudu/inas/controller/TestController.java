29373c30951026a1db0711aaa8c3f32844512a88
姜高宇 <1531115737@qq.com>
2018-02-28 20:23:31 +0800

    ninth commit

--- src/main/java/com/sudu/inas/controller/TestController.java
+++ src/main/java/com/sudu/inas/controller/TestController.java
@@ -1,11 +1,134 @@
 package com.sudu.inas.controller;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import com.sudu.inas.beans.*;
+import com.sudu.inas.service.ConnectionService;
+import com.sudu.inas.service.ObjectService;
+import com.sudu.inas.service.RawinfoService;
+import com.sudu.inas.util.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.rmi.MarshalledObject;
+import java.util.*;
+
+
+@Controller
 public class TestController {
 
-    public static void main(String[] args) {
+    @Autowired
+    ObjectService objectService;
+
+    @Autowired
+    ConnectionService connectionService;
+
+    @Autowired
+    RawinfoService rawinfoService;
+
+
+    @RequestMapping(value = "/graphtest.do",method = RequestMethod.POST)
+    public @ResponseBody
+    Map<String, ArrayList> getSingLineGraph(String objectId) throws Exception {
+        Map<String, ArrayList> map = singLineGraphforId(objectId,0,null);
+        return map;
     }
+
+    @RequestMapping(value = "/graphforconn.do",method = RequestMethod.POST)
+    public @ResponseBody
+    Map<String,ArrayList> getConnLine(String objectId,String selectedId,int num) throws Exception {
+        System.out.println(objectId);
+        System.out.println(selectedId);
+        System.out.println(num);
+        Map<String, ArrayList> map = singLineGraphforId(selectedId,num,objectId);
+        return map;
+    }
+
+
+    @RequestMapping(value = "/searchtest.do",method = RequestMethod.POST)
+    public @ResponseBody
+    Map<String,String> getDesLlist(String prefix) throws Exception {
+        HashMap<String, String> desList = new HashMap<>();
+        List<Entity> objectsByPrefix = objectService.findObjectsByPrefix(prefix);
+        for (Entity entity:objectsByPrefix){
+            desList.put(entity.getObjectId(),entity.getRealName());
+        }
+        return desList;
+    }
+    @RequestMapping(value = "/saveNewEntitytest.do",method = RequestMethod.POST)
+    public @ResponseBody
+    Map<String,String> saveNewEntity(String name,String description,String prefix) throws Exception {
+        String objectId = name + CommonUtil.genRandomNum();
+        String realName = name+" "+description;
+        rawinfoService.addRealName(realName,objectId);
+        HashMap<String, String> desList = new HashMap<>();
+        List<Entity> objectsByPrefix = objectService.findObjectsByPrefix(prefix);
+        for (Entity entity:objectsByPrefix){
+            desList.put(entity.getObjectId(),entity.getRealName());
+        }
+        return desList;
+    }
+
+    public Map<String, ArrayList> singLineGraphforId(String objectId,int num,String connId) throws Exception {
+        HashMap<String, ArrayList> map = new HashMap<>();
+        ArrayList<Edge> edges = new ArrayList<>();
+        ArrayList<Node> nodes = new ArrayList<>();
+        ArrayList<String> idList = new ArrayList<>();
+        int xBegin = 5;
+        int yBegin = 5;
+        int x = xBegin;
+        int y = yBegin + num*5;
+        Entity entity = objectService.findObjectById(objectId);
+        if (null == entity){
+            return null;
+        }
+        ArrayList<Timenode> timeLine = entity.getTimeLine();
+        for (Timenode timenode:timeLine){
+            Node node = new Node();
+            if (num == 0){
+                node.setColor("red");
+            }else {
+                node.setColor("blue");
+            }
+            node.setSize(20);
+            node.setLabel(timenode.getTimePoint()+timenode.getInfo());
+            String id = objectId+timenode.getTimePoint();
+            idList.add(id);
+            node.setId(objectId+timenode.getTimePoint());
+            node.setY(y);
+            x = x+10;
+            node.setX(x);
+            nodes.add(node);
+            if (connId!= null){
+                List<Connection> conncetionListByTimePoint = connectionService.findConncetionListByTimePoint(objectId, timenode.getTimePoint());
+                if (conncetionListByTimePoint!= null){
+                    for (Connection connection:conncetionListByTimePoint){
+                        if (connection.getConnObjectId().equals(connId)) {
+                            Edge edge = new Edge(id, connId + connection.getConntimePoint());
+                            edges.add(edge);
+                        }
+
+                    }
+                }
+            }
+        }
+        if (idList.size()>1){
+            Iterator<String> idIter = idList.iterator();
+            String exid = idIter.next();
+            while (idIter.hasNext()){
+                String id = idIter.next();
+                Edge edge = new Edge(exid, id);
+                edges.add(edge);
+                exid = id;
+            }
+        }
+
+        map.put("nodes", nodes);
+        map.put("edges",edges);
+        return map;
+    }
+
+
 }
--- src/main/resources/static/js/extract.js
+++ src/main/resources/static/js/extract.js
@@ -1,5 +1,17 @@
 var myChart = echarts.init(document.getElementById('select_area'));
+var initialNodes= [];
+var initialEdges= [];
+var allNodes = [];
+var allEdges= [];
+var newNodes= [];
+var newEdges= [];
+var selected = {}
+var deslist = {}
+var selectedNum = 0;
+var pre = "dd";
+var selectlist  = [];
 $(function () {
+
     $("#analysis").click(function () {
         $.ajax({
             type: "post",
@@ -17,56 +29,194 @@ $(function () {
             data: {objectId: $('#objectId').val()},
             dataType: "json",
             success: function (result) {
-                document.getElementById("edit").style.display= "block";
-                setGraphParas(result);
+                document.getElementById("edit").style.display = "block";
+                initialNodes = result.nodes;
+                allNodes = result.nodes;
+                allEdges = result.edges;
+                initialEdges = result.edges;
+                setGraphParas(allNodes,allEdges);
             }
         });
     });
-    $("#extractresult").on("click","a",function () {
-        var line ="<tr>"
-            +"<td class=\"col-md-2\">" + "<input class=\"form-control\"/>" +"</td>"
-            +"<td class=\"col-md-2\">"+ "<input  class=\"form-control\"/>"  + "</td>"
-            +"<td class=\"col-md-1\">"+ "<input  class=\"form-control\"/>"  + "</td>"
-            +"<td class=\"col-md-6\">"+ "<input  class=\"form-control\"/>"  + "</td>"
-            +"<td class=\"col-md-2\">"+ "<button class=\"addToPic\">添加</button>"  + "</td>"
-            +"</tr>";
+    $("#modelFooter").on("click","#toSearch",function () {
+        pre = $('#prefix').val();
+        console.log(pre)
+        $.ajax({
+            type: "post",
+            url: "/searchtest.do",
+            data: {prefix: $('#prefix').val()},
+            dataType: "json",
+            success: function (result) {
+                console.log(result);
+                createDesTable(result)
+
+            }
+        });
+    });
+    $("#extractresult").on("click", "#addNewLine", function () {
+        var line = "<tr>"
+            + "<td class=\"col-md-2\">" + "<input class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-2\">" + "<input  class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-1\">" + "<input  class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-6\">" + "<input  class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-2\">" + "<button class=\"addNewNode\">添加</button>" + "</td>"
+            + "</tr>";
         $("#resultTable").append(line);
-    })
+    });
+    $("#modelBody").on("click", "#addNewEntity", function () {
+        var line = "<div id=\"newform\" class=\"form-inline\">\n" +
+            "        <div class=\"form-group\">\n" +
+            "            <label for=\"name\">Name</label>\n" +
+            "            <input class=\"form-control\" id=\"name\" placeholder=\"Jane Doe\"/>\n" +
+            "        </div>\n" +
+            "        <div class=\"form-group\">\n" +
+            "            <label for=\"description\">Description</label>\n" +
+            "            <input class=\"form-control\" id=\"description\" placeholder=\"clown\"/>\n" +
+            "        </div>\n" +
+            "    </div>" +
+            "        <button id=\"savenew\"  class=\"btn btn-primary\">Add</button>\n";
+        $("#modelBody").append(line);
+    });
+    $("#modelBody").on("click", "#savenew", function () {
+        var nameVal = $('#name').val()
+        var desVal = $('#description').val()
+        var prefix = pre
+        $.ajax({
+            type: "post",
+            url: "/saveNewEntitytest.do",
+            data: {name: nameVal, description: desVal, prefix: prefix},
+            // dataType: "json",
+            success: function (result) {
+                console.log(result);
+                createDesTable(result);
+            }
+        });
+    });
+    $('#myModal').on('hide.bs.modal', function () {
+        var line =
+        "<form class=\"form-inline\">\n" +
+        "         <div class=\"form-group\">\n" +
+        "                <label for=\"prefix\">Keyword</label>\n" +
+        "                <input name=\"prefix\" class=\"form-control\" id=\"prefix\" placeholder=\"Michael Jackson\"/>\n" +
+        "         </div>\n" +
+        "</form>";
+        $("#modelBody").html(line);
+        var line2 = "<button type=\"button\" id=\"toSearch\" class=\"btn btn-primary\">Search</button>\n" +
+            "        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>";
+        $("#modelFooter").html(line2);
+    });
+
+    $("#modelFooter").on("click", "#select", function () {
+        var radio_ary = document.getElementsByName("test1");
+
+        for (var i = 0; i < radio_ary.length; i++) {
+            if (radio_ary[i].checked == true) { //得到选中的单选按钮状态（判断是否被选中）
+                var parent = radio_ary[i].parentNode.parentNode;
+            }
+        }
+
+        var single = parent.childNodes;
+        var id = single[0].innerText;
+        var des = single[1].innerText;
+        selected[id] = des;
+        deslist[selectedNum] = des;
+        selectedNum = selectedNum+1;
+        selectlist.push(id);
+        createSelectedTable();
+        $.ajax({
+            type: "post",
+            url: "/graphforconn.do",
+            data: {objectId: $("#objectId").val(), selectedId: id, num: selectedNum},
+            // dataType: "json",
+            success: function (result) {
+                console.log(result);
+                allNodes = allNodes.concat(result.nodes);
+                allEdges = allEdges.concat(result.edges);
+                console.log(allNodes);
+                setGraphParas(allNodes,allEdges);
+            }
+        });
+    });
+    $("#extractresult").on("click", ".addNewNode", function () {
+            alert("aaaaa");
+    });
+
 });
+
+function createSelectedTable(){
+    var tableStr = "<table class=\"table table-striped\">";
+    tableStr = tableStr
+        + "<tr>"
+        + "<th>序号</th>"
+        + "<th>描述</th>"
+        + "</tr>";
+    for (var key in deslist) {
+        tableStr = tableStr + "<tr>"
+            + "<td class=\"col-md-2\">" + key + "</td>"
+            + "<td class=\"col-md-3\">" +deslist[key] + "</td>"
+            + "</tr>"
+    }
+    tableStr = tableStr + "</table>";
+    $("#selectedEntities").html(tableStr);
+}
+
+function createDesTable(result) {
+    var tableStr = "<table class=\"table table-striped\">";
+    var add = "<a id=\"addNewEntity\" class=\"text-right\">Create a new Entity</a>"
+    tableStr = tableStr
+        + "<tr>"
+        + "<th>ID</th>"
+        + "<th>描述</th>"
+        + "<th>选定</th>"
+        + "</tr>";
+    for (var key in result) {
+        tableStr = tableStr + "<tr>"
+            + "<td class=\"col-md-2\">" + key + "</td>"
+            + "<td class=\"col-md-3\">" + result[key] + "</td>"
+            + "<td class=\"col-md-1\">" + "<input name='test1' type=\"radio\" class=\"select2\" value=\"\" />" + "</td>"
+            + "</tr>"
+    }
+    tableStr = tableStr + "</table>";
+    $("#modelBody").html(tableStr);
+    $("#modelBody").append(add);
+    var line = "<button id=\"select\"  class=\"btn btn-primary\">Add</button>";
+    $("#modelFooter").html(line);
+
+}
 
 function createExtractTable(data) {
     var addnew = "<tr>" +
         "<td><a id=\"addNewLine\">Add New Line</a></td>" +
         "</tr>";
-    var panelStr = " <div class=\"panel panel-primary\" >\n" +
+    var panelStr = " <div class=\"panel panel-success\" >\n" +
         "<div class=\"panel-heading\">Extract Result</div>\n" +
         "<form>\n" +
         "<div class=\"panel-body\">\n";
     var tableStr = panelStr + "<table class=\"table table-striped\" id=\"resultTable\">";
     tableStr = tableStr
         + "<tr>"
-        +"<th>实体名</th>"
-        +"<th>时间点</th>"
-        +"<th>地点</th>"
-        +"<th>描述</th>"
-        +"<th>添加</th>"
-        +"</tr>";
+        + "<th>实体名</th>"
+        + "<th>时间点</th>"
+        + "<th>地点</th>"
+        + "<th>描述</th>"
+        + "<th>添加</th>"
+        + "</tr>";
     var len = data.length;
-    for ( var i = 0; i < len; i++) {
+    for (var i = 0; i < len; i++) {
         tableStr = tableStr + "<tr>"
-            +"<td class=\"col-md-2\">" + "<input value="+data[i].entityName +" class=\"form-control\"/>" +"</td>"
-            +"<td class=\"col-md-2\">"+ "<input value="+data[i].timepoint +" class=\"form-control\"/>"  + "</td>"
-            +"<td class=\"col-md-1\">"+ "<input value="+data[i].location +" class=\"form-control\"/>"  + "</td>"
-            +"<td class=\"col-md-6\">"+ "<input value="+data[i].description +" class=\"form-control\"/>"  + "</td>"
-            +"<td class=\"col-md-2\">"+ "<button class=\"addToPic\">添加</button>"  + "</td>"
-            +"</tr>";
+            + "<td class=\"col-md-2\">" + "<input value=" + data[i].entityName + " class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-2\">" + "<input value=" + data[i].timepoint + " class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-1\">" + "<input value=" + data[i].location + " class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-6\">" + "<input value=" + data[i].description + " class=\"form-control\"/>" + "</td>"
+            + "<td class=\"col-md-2\">" + "<button class=\"addNewNode\">添加</button>" + "</td>"
+            + "</tr>";
     }
-    tableStr = tableStr + "</table>"+ addnew +"</div>" + "</form>" + "</div>";
+    tableStr = tableStr + "</table>" + addnew + "</div>" + "</form>" + "</div>";
     //添加到div中
     $("#extractresult").html(tableStr);
 }
 
-function setGraphParas(result) {
+function setGraphParas(nodes,edges) {
     myChart.setOption({
         title: {
             text: '                                      Object Dependencies'
@@ -78,7 +228,7 @@ function setGraphParas(result) {
                 type: 'graph',
                 layout: 'none',
                 // progressiveThreshold: 700,
-                data: result.nodes.map(function (node) {
+                data: nodes.map(function (node) {
                     return {
                         x: node.x,
                         y: node.y,
@@ -92,7 +242,7 @@ function setGraphParas(result) {
                         }
                     };
                 }),
-                edges: result.edges.map(function (edge) {
+                edges: edges.map(function (edge) {
                     return {
                         source: edge.sourceID,
                         target: edge.targetID
