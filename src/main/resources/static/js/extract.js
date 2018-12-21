f320e67b1dc82bac5247ea376b05f15af7a25a72
姜高宇 <1531115737@qq.com>
2018-12-21 17:29:20 +0800

    19th commit

--- src/main/resources/static/js/extract.js
+++ src/main/resources/static/js/extract.js
@@ -180,6 +180,8 @@ $(function () {
         var timePoint = single[1].firstChild.value;
         var location = single[2].firstChild.value;
         var description = single[3].firstChild.value;
+        var r = new RegExp("(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)");
+        var bool = r.test(timePoint);
 
         if (bool){
             var time = timePoint.split("-");
